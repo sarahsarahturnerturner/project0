3484ce287749e3f2d01aacfa0897e79ee52d6351
Sarah Turner <sarahturner@Sarahs-MBP-2.domain>
2021-01-19 11:23:06 -0800

    added moon and funky water ppeaks

--- sketch.js
+++ sketch.js
@@ -4,6 +4,11 @@ background(252, 83, 252);
 noStroke();
 fill(252);
 
+ beginShape();
+     fill(147,237,234);
+    ellipse(1800, 200, 200,200);
+    endShape();
+
 beginShape();
 fill(219,150,235);
  vertex(0,height/2);
@@ -25,18 +30,20 @@ fill(219,150,235);
   beginShape();
     fill(147,237,234);
     vertex(width*8/24,height/2);
-    bezierVertex(width*8/24,height*17/32,  width*8/24,height*19/32,   width*9/24,height*19/32);//peak1-left
-    bezierVertex(width*10/24,height*19/32,  width*9/24,height*17/32  ,width*10/24,height*17/32);//peak1-right
-    bezierVertex(width*11/24,height*17/32,  width*19/48,height*23/32  ,width*11/24,height*23/32);//peak2-left
-    bezierVertex(width*12/24,height*23/32,  width*11/24,height*21/32  ,width*12/24,height*21/32);//peak2-right
-    bezierVertex(width*13/24,height*21/32,  width*11/24,height*29/32  ,width*13/24,height*29/32);//peak3-left
-    bezierVertex(width*29/48,height*29/32,  width*13/24,height*19/32  ,width*14/24,height*19/32);//peak3-right
-    bezierVertex(width*15/24,height*19/32,  width*14/24,height*22/32  ,width*15/24,height*22/32);//peak4-left
-    bezierVertex(width*16/24,height*22/32,  width*16/24,height*19/32  ,width*16/24,height/2);//peak4-right
+    bezierVertex(width*6/24,height*17/32,  width*8/24,height*17/32,   width*9/24,height*19/32);//peak1-left
+    bezierVertex(width*8/24,height*19/32,  width*9/24,height*15/32  ,width*10/24,height*17/32);//peak1-right
+    bezierVertex(width*9/24,height*17/32,  width*19/48,height*21/32  ,width*11/24,height*23/32);//peak2-left
+    bezierVertex(width*10/24,height*23/32,  width*11/24,height*19/32  ,width*12/24,height*21/32);//peak2-right
+    bezierVertex(width*11/24,height*21/32,  width*11/24,height*27/32  ,width*13/24,height*29/32);//peak3-left
+    bezierVertex(width*27/48,height*29/32,  width*13/24,height*17/32  ,width*14/24,height*19/32);//peak3-right
+    bezierVertex(width*13/24,height*19/32,  width*14/24,height*20/32  ,width*15/24,height*22/32);//peak4-left
+    bezierVertex(width*14/24,height*22/32,  width*16/24,height*17/32  ,width*16/24,height/2);//peak4-right
     vertex(width*8/24,height/2);
   endShape();
 
   noLoop();
+
+
 }
 
 function draw() {
