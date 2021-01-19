6d8760cd98a95c4054ce6753243edfd20c84cf4e
Sarah Turner <sarahturner@Sarahs-MBP-2.domain>
2021-01-19 11:02:43 -0800

    ice cream color pallette and added sharp and wet content

--- sketch.js
+++ sketch.js
@@ -1,7 +1,48 @@
 function setup() {
-createCanvas(800,800);
+createCanvas(1920,1080);
+background(252, 83, 252);
+noStroke();
+fill(252);
+
+beginShape();
+fill(219,150,235);
+ vertex(0,height/2);
+    vertex(width*8/24,height/2);
+    vertex(width*9/24,height*11/32);//peak1
+    vertex(width*10/24,height*13/32);
+    vertex(width*11/24,height*3/32);//peak2
+    vertex(width*12/24,height*8/32);
+    vertex(width*13/24,height*0/32);//peak3
+    vertex(width*14/24,height*10/32);
+    vertex(width*15/24,height*8/32);//peak4
+    vertex(width*16/24,height/2);
+    vertex(width,height/2);
+    vertex(width,height);
+    vertex(0,height);
+    vertex(0,height/2);
+  endShape();
+
+  beginShape();
+    fill(147,237,234);
+    vertex(width*8/24,height/2);
+    bezierVertex(width*8/24,height*17/32,  width*8/24,height*19/32,   width*9/24,height*19/32);//peak1-left
+    bezierVertex(width*10/24,height*19/32,  width*9/24,height*17/32  ,width*10/24,height*17/32);//peak1-right
+    bezierVertex(width*11/24,height*17/32,  width*19/48,height*23/32  ,width*11/24,height*23/32);//peak2-left
+    bezierVertex(width*12/24,height*23/32,  width*11/24,height*21/32  ,width*12/24,height*21/32);//peak2-right
+    bezierVertex(width*13/24,height*21/32,  width*11/24,height*29/32  ,width*13/24,height*29/32);//peak3-left
+    bezierVertex(width*29/48,height*29/32,  width*13/24,height*19/32  ,width*14/24,height*19/32);//peak3-right
+    bezierVertex(width*15/24,height*19/32,  width*14/24,height*22/32  ,width*15/24,height*22/32);//peak4-left
+    bezierVertex(width*16/24,height*22/32,  width*16/24,height*19/32  ,width*16/24,height/2);//peak4-right
+    vertex(width*8/24,height/2);
+  endShape();
+
+  noLoop();
 }
 
 function draw() {
-ellipse(mouseX, mouseY, 100, 100);
+
+
+
+
+
 }
